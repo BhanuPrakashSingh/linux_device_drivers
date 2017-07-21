b19f39dde194dc3858946787f80fb75727c3d82b
pragna <danthuripragna@gmail.com>
2017-07-21 16:29:06 +0530

    module program on kthread using semaphore

---
+++ pragna/kthread/.kthread.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/bhumi/Desktop/concurrency/kthread/kthread.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/bhumi/Desktop/concurrency/kthread/kthread.ko /home/bhumi/Desktop/concurrency/kthread/kthread.o /home/bhumi/Desktop/concurrency/kthread/kthread.mod.o
